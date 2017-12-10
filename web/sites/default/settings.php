1a3ceff99562bd9e39b339d55c24591504411567
devkodama <33792590+devkodama@users.noreply.github.com>
2017-12-10 16:23:25 -0600

    Created basic_page and directory_entry content types.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -46,3 +46,6 @@ if (file_exists($app_root . '/' . $site_path . '/settings.lando.php')) {
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
+
+// Define settings for local dev environment
+$settings['file_private_path'] = 'sites/default/files/private';
